c9ed92ac6b17102cde2ba4b00c9c010a9b187c7c
Steven Liu <xian.f.liu@gmail.com>
2019-05-10 14:41:29 +0100

    Added test for hash_160

--- test/transactions/SecretLockTransaction.spec.js
+++ test/transactions/SecretLockTransaction.spec.js
@@ -26,7 +26,7 @@ describe('SecretLockTransaction', () => {
 		privateKey: '041e2ce90c31cd65620ed16ab7a5a485e5b335d7e61c75cd9b3a2fed3e091728'
 	};
 
-	it('should create secret lock transaction', () => {
+	it('should create secret lock transaction - OP_HASH_160 (short hash)', () => {
 		const hash = sha3_512.create();
 		hash.update('secret');
 		const secretLockTransaction = {
@@ -57,5 +57,37 @@ describe('SecretLockTransaction', () => {
 			'225FE6D12B73A7D51F2992CE82951DBF8C173FA40000000000' +
 			'0000000000000090E8FEBD671DD41BEE94EC3BA5831CB608A3' +
 			'12C2F203BA84AC');
+	});
+	it('should create secret lock transaction', () => {
+		const hash = sha3_512.create();
+		hash.update('secret');
+		const secretLockTransaction = {
+			deadline: deadline(),
+			mosaicId: [3646934825, 3576016193],
+			mosaicAmount: uint64.fromUint(10000000),
+			duration: uint64.fromUint(100),
+			hashAlgorithm: 0,
+			secret: 'f66481fa5a03660fe655cfbcd517cb47ed60e42a47ed60e42a3660fe655cfbc3',
+			recipient: 'SDUP5PLHDXKBX3UU5Q52LAY4WYEKGEWC6IB3VBFM'
+		};
+		const verifiableTransaction = new SecretLockTransaction.Builder()
+			.addDeadline(secretLockTransaction.deadline)
+			.addMosaicId(secretLockTransaction.mosaicId)
+			.addMosaicAmount(secretLockTransaction.mosaicAmount)
+			.addDuration(secretLockTransaction.duration)
+			.addHashAlgorithm(secretLockTransaction.hashAlgorithm)
+			.addSecret(secretLockTransaction.secret)
+			.addRecipient(secretLockTransaction.recipient)
+			.build();
+
+		const transactionPayload = verifiableTransaction.signTransaction(keyPair);
+
+		expect(transactionPayload.payload.substring(
+			240,
+			transactionPayload.payload.length
+		)).to.be.equal('29CF5FD941AD25D580969800000000006400000' +
+			'00000000000F66481FA5A03660FE655CFBCD517CB47ED60E42' +
+			'A47ED60E42A3660FE655CFBC390E8FEBD671DD41BEE94EC3BA' +
+			'5831CB608A312C2F203BA84AC');
 	});
 });
